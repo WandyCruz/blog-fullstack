507351a32171c702bb9c4be17e8034cd4f55962f
Wandy Cruz <lopezwandy68@.com>
2025-08-06 19:20:13 -0400

    mejoras de seguridad en las rutas

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'https://tu-frontend-url.com',
-    Credential: true,
+    credentials: true,
   });
   app.use(cookieParser());
   await app.listen(process.env.PORT || 3000);
