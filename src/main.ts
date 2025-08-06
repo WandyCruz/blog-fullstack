990dc2880b4abb3e3c2c82dcae37192af77afee4
Wandy Cruz <lopezwandy68@.com>
2025-08-06 19:04:22 -0400

    mejoras de seguridad en las rutas

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3000);
   console.log('servidor correindo en' + ' ' + 'http://localhost:3001');
 }
 
