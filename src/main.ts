cc67d8f5f0c4928443930bcffc4d842f9885ac16
Wandy Cruz <lopezwandy68@.com>
2025-08-06 19:07:32 -0400

    mejoras de seguridad en las rutas

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://tu-frontend-url.com',
+    Credential: true,
+  });
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
   console.log('servidor correindo en' + ' ' + 'http://localhost:3001');
